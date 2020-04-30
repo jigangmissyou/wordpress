70332c8b197cd95431c5c5de303fe8f06d143597
guojigang <707394840@qq.com>
2020-04-30 18:02:57 +0800

    updage

--- wp-config.php
+++ wp-config.php
@@ -75,7 +75,7 @@ $table_prefix = 'wp_';
  *
  * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
-define('WP_DEBUG', true);
+define('WP_DEBUG', false);
 
 /* 好了！请不要再继续编辑。请保存本文件。使用愉快！ */
 
